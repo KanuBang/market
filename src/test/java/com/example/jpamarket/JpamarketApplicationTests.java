a7351170a7f96270b6093826c5d3d13c20b75079
gdrffg <gdrffg@gmail.com>
2024-10-14 17:24:13 +0900

    initial commit

---
+++ src/test/java/com/example/jpamarket/JpamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpamarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
